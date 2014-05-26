9ac8554bc27d38695cf2733a72798744172eac2b
mrmacinnes <m.macinnes@trinity.nottingham.sch.uk>
2014-05-26 22:14:35 +0100

    No change

--- js/_vti_cnf/game_manager.js
+++ js/_vti_cnf/game_manager.js
@@ -1,9 +1,9 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|26 May 2014 11:33:46 -0000
+vti_timelastmodified:TR|26 May 2014 20:51:26 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|index.html
 vti_author:SR|DUO11\\Mark
 vti_modifiedby:SR|DUO11\\Mark
 vti_timecreated:TR|26 May 2014 18:28:10 -0000
-vti_cacheddtm:TX|26 May 2014 18:28:10 -0000
+vti_cacheddtm:TX|26 May 2014 20:51:26 -0000
 vti_filesize:IR|7899
